c211a1f393e35797191d516988737a13060ed3c1
inesfreitas <ines.freitas@zonmail.pt>
2023-05-25 22:08:08 +0100

    JS

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+
+
+/*for (var i = 1; i<9; i++ ){
+    console.log( "Exercício 1 Repetição " + i + " blabla");
+};*/
+
+var anchor = document.querySelector("#main-navigation").querySelectorAll("a");
+
+for( var i = 0; i < anchor.length; i++){
+    if(window.location.href == anchor[i].href) {
+        anchor[i].classList.add("active");
+    }
+
+    if(window.location.href.indexOf("work") > -1) {
+        console.log("estou na página work")
+        document.body.classList.add("work")
+    } else if (window.location.href.indexOf("contacts") > -1){
+        document.body.classList.add("contacts")
+    } else {
+    document.body.classList.add("home")
+}
+}
